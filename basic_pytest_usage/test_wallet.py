6233b383cd0ed4e16c052ede1656fe354d27de1a
=Isaac <=trevex2000@live.com>
2017-08-15 13:17:36 +0300

    use of fixtures in testing for file test_wallet.py

--- basic_pytest_usage/test_wallet.py
+++ basic_pytest_usage/test_wallet.py
@@ -2,25 +2,28 @@ import pytest
 
 from wallet import Wallet, InsufficientBalance
 
-def test_default_initial_balance():
-    wallet = Wallet()
-    assert wallet.balance == 0
+@pytest.fixture
+def empty_wallet():
+    return Wallet()
 
-def test_setting_initial_balance():
-    wallet = Wallet(100)
-    assert wallet.balance == 100
+@pytest.fixture
+def wallet():
+    return Wallet(50)
 
-def test_wallet_add_cash():
-    wallet = Wallet(10)
-    wallet.add_cash(90)
-    assert wallet.balance == 100
+def test_default_initial_balance(empty_wallet):
+        assert empty_wallet.balance == 0
 
-def test_wallet_spend_cash():
-    wallet = Wallet(30)
+def test_setting_initial_balance(wallet):
+    assert wallet.balance == 50
+
+def test_wallet_add_cash(empty_wallet):
+    empty_wallet.add_cash(40)
+    assert empty_wallet.balance == 40
+
+def test_wallet_spend_cash(wallet):
     wallet.spend_cash(10)
-    assert wallet.balance == 20
+    assert wallet.balance == 40
 
-def test_wallet_spend_cash_raises_insufficient_balance_exception():
-    wallet = Wallet()
+def test_wallet_spend_cash_raises_insufficient_balance_exception(empty_wallet):
     with pytest.raises(InsufficientBalance):
-        wallet.spend_cash(100)
+        empty_wallet.spend_cash(100)
